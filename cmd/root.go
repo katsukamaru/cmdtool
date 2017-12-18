782b01a4e593697846f648b84198d3a369551528
shin katsumaru <ms.katsumaru@gmail.com>
2017-12-18 23:10:05 +0900

    add version

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,31 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+const VERSION = "0.0.1"
+
+var RootCmd = &cobra.Command{
+	Use:   "cmdtool",
+	Short: "cmdtool",
+	Long:  "Command tool",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	cobra.OnInitialize()
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of cmdtool",
+	Long:  `All software has versions.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("cmdtool v" + VERSION)
+	},
+}
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"cmdtools/cmd"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Println("hello world")
-	os.Exit(0)
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 }
-
