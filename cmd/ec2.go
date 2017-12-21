4c861654779ced0d8a4cd46c7b071d89eebd2e23
shin katsumaru <ms.katsumaru@gmail.com>
2017-12-21 09:37:57 +0900

    add ec2 list

--- cmd/ec2.go
+++ cmd/ec2.go
@@ -1,12 +1,8 @@
 package cmd
 
 import (
+	"github.com/katsukamaru/cmdtool/cmd/ec2"
 	"github.com/spf13/cobra"
-
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func init() {
@@ -18,37 +14,6 @@ var ec2Cmd = &cobra.Command{
 	Short: "List of instances.",
 	Long:  "List of instances.",
 	Run: func(cmd *cobra.Command, args []string) {
-		doSomething()
+		ec2.List()
 	},
-}
-
-func doSomething() {
-	svc := ec2.New(
-		session.New(),
-		&aws.Config{
-			Region: aws.String("ap-northeast-1"),
-		},
-	)
-
-	params := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
-			{
-				Name: aws.String("instance-state-name"),
-				Values: []*string{
-					aws.String("running"),
-				},
-			},
-		},
-	}
-
-	resp, err := svc.DescribeInstances(params)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, res := range resp.Reservations {
-		for _, inst := range res.Instances {
-			fmt.Println(inst)
-		}
-	}
 }
